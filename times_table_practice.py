f7cd18e336e9fe815c63606e03187e911228631e
mthorntonhanks <80684095+mthorntonhanks@users.noreply.github.com>
2022-09-25 22:54:51 +0100

    Add files via upload

---
+++ times_table_practice.py
@@ -0,0 +1,40 @@
+'''
+To do
+-----
+Research best/latest string format method
+Research best random integer method (randint?)
+'''
+
+import random
+
+correct_score = 0
+
+for question_number in range(1, 6):
+    #Could ask user if they want easy or difficult mode (lower or higher numbers)
+    a = random.randrange(13)
+    b = random.randrange(13)
+
+    print(f'Question {question_number} - what is {a} x {b}?')
+    user_answer = input()
+
+    if int(user_answer) == (a * b):
+        #Could change the scores so that you get 10 points for a correct answer
+        correct_score += 1
+        print('Correct! :)')
+    else:
+        #Could tell the user the correct answer
+        #Could change scores so you get points deducted for wrong answer
+        print('Wrong! :(')
+
+print(f'You scored {correct_score} out of 5')
+if correct_score == 0:
+    print('Don''t give up the day job!')
+elif correct_score in [1, 2]:
+    #Could change this to score == 1 or score == 2
+    print('Could do better!')
+elif correct_score in [3, 4]:
+    print('Not bad!')
+elif correct_score == 5:
+    #Could change this to 'else'
+    print('Great work!')
+
